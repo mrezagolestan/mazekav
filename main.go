ec332f601796082a1d89c476458d9d9dc0f71467
Mohammad Reza Golestan <mreza_golestan@live.com>
2024-02-05 18:31:51 +0330

    first commit (docker compose for mysql) + gorm mysql connect + env

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"os"
+)
+
+func main() {
+	godotenv.Load()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DB"),
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	fmt.Println(db, err)
+}
